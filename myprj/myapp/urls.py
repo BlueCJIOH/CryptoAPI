c252941d1b568818062e06bac82bd60a8af408e0
Gleb Lukyanov <gleb.lukyanov@el-pixel.com>
2022-11-11 18:05:40 +0300

    Installation and setting swagger

--- myprj/myapp/urls.py
+++ myprj/myapp/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import SimpleRouter
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from .views import ChangeViewSet, CurrencyViewSet
 
@@ -7,7 +10,19 @@ router = SimpleRouter()
 router.register(r'changes', ChangeViewSet)
 router.register(r'currencies', CurrencyViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Crypto API",
+        default_version='v1',
+        description="All changes with crypto currencies",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('', include(router.urls)),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
