ac029c3edef0a505fc83243a9d0872a1bb4738ab
Gleb Lukyanov <gleb.lukyanov@el-pixel.com>
2022-12-15 18:47:44 +0300

    Added new services and endpoing to get all data from exchanges.

--- myprj/myapp/views.py
+++ myprj/myapp/views.py
@@ -1,31 +1,59 @@
-from django.shortcuts import render
 # 804DDCE9-9BBE-45DE-A5F2-BCA7D4C58400
-# Create your views here.
-from rest_framework import viewsets, mixins, views
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.generics import GenericAPIView
 from rest_framework.viewsets import GenericViewSet
 
-from .serializers import ExchangeSerializer
-from .services.services import get_all_exchanges, get_exchange_by_name
+from .serializers import ExchangeSerializer, CurrencySerializer, ExchangeCurrencySerializer
+from .services.services import get_all_exchanges, get_exchange_by_name, \
+    get_bybit_by_currency, get_binance_by_currency, get_exchange_by_currency, get_names_from_exchange, \
+    get_names_from_exchanges
 
 
 class ExchangeViewSet(GenericViewSet):
-    serializer_class = ExchangeSerializer
     exchanges = (
         'Binance',
         'Bybit',
     )
+    default_serializer_class = ExchangeSerializer
+    serializer_classes = {
+        'list': CurrencySerializer,
+        'all_currencies': CurrencySerializer,
+        'currencies_by_name': ExchangeSerializer,
+        'all_currency_by_name': CurrencySerializer,
+        'currencies_names_from': ExchangeSerializer,
+        'currency_by_name_from': ExchangeCurrencySerializer,
+    }
+
+    def get_serializer_class(self):
+        return self.serializer_classes.get(self.action, self.default_serializer_class)
 
     def list(self, request):
         return Response([{el[0]: el[1]} for el in tuple(zip(['name', ] * len(self.exchanges), self.exchanges))])
 
-    @action(detail=False, methods=['get'], url_path='get_all_currencies')
-    def get_all_currencies(self, request):
+    @action(detail=False, methods=['get'], url_path='all_currencies')
+    def all_currencies(self, request):
         content = [{'Binance': get_all_exchanges()['Binance']}, {'Bybit': get_all_exchanges()['Bybit']}]
         return Response(content)
 
-    @action(detail=False, methods=['post'], url_path='get_currencies_by_name', )
-    def get_currencies_by_name(self, request):
+    @action(detail=False, methods=['post'], url_path='currencies_by_name')
+    def currencies_by_name(self, request):
         return Response({request.data['name']: get_exchange_by_name(request.data['name'].lower())})
+
+    @action(detail=False, methods=['post'], url_path='all_currency_by_name')
+    def all_currency_by_name(self, request):
+        content = [{'Binance': get_bybit_by_currency(request.data['cname'].upper())},
+                   {'Bybit': get_binance_by_currency(request.data['cname'].upper())}]
+        return Response(content)
+
+    @action(detail=False, methods=['post'], url_path='currency_by_name_from')
+    def currency_by_name_from(self, request):
+        return Response(
+            {request.data['name']: get_exchange_by_currency(request.data['name'].lower(), request.data['cname'].upper())})
+
+    @action(detail=False, methods=['post'], url_path='currencies_names_from')
+    def currency_names_from(self, request):
+        return Response({request.data['name']: get_names_from_exchange(request.data['name'].lower())})
+
+    @action(detail=False, methods=['get'], url_path='all_currency_names')
+    def all_currency_names(self, request):
+        return Response(get_names_from_exchanges())
